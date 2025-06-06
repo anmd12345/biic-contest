e567a288fc1ae2323b6f21fb00085bf1d36e20a3
anmd12345 <anvohoang98@gmail.com>
2025-06-07 02:57:19 +0700

    ui: create partial layout

---
+++ BIIC-Contest/Controllers/PartialController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BIIC_Contest.Controllers
+{
+    public class PartialController : Controller
+    {
+        public ActionResult NavbarPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult SupportChatPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult SettingCanvasPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult SearchModalPartial()
+        {
+            return PartialView();
+        }
+
+
+        public ActionResult Footer1Partial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Footer2Partial()
+        {
+            return PartialView();
+        }
+    }
+}
