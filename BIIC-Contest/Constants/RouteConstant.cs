2cd5a6eca15bdae2375e64e9107467af1d684247
anmd12345 <anvohoang98@gmail.com>
2025-06-07 02:41:05 +0700

    fix: fix duplicate footer at contact page

---
+++ BIIC-Contest/Constants/RouteConstant.cs
@@ -0,0 +1,8 @@
+﻿namespace BIIC_Contest.Constants
+{
+    public class RouteConstant
+    {
+        public const string HOME_PAGE = "/";
+        public const string CONTACT_PAGE = "/Home/Contact";
+    }
+}
