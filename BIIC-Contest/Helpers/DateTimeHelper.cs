de5e51e747e33f50812c5fa1b613a87fc66fd30e
anmd12345 <anvohoang98@gmail.com>
2025-06-07 18:16:28 +0700

    create project structure

---
+++ BIIC-Contest/Helpers/DateTimeHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BIIC_Contest.Helpers
+{
+    public class DateTimeHelper
+    {
+        // Định dạng lại thời gian hiện tại theo dạng "dd/MM/yyyy-HH:mm"
+        public string getFormattedDateNow()
+        {
+            return DateTime.Now.ToString("dd/MM/yyyy-HH:mm");
+        }
+    }
+}
