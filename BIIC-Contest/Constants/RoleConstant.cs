32a509d850a0eb5f136fab3156bcc20bdf32306c
anmd12345 <anvohoang98@gmail.com>
2025-06-08 05:12:38 +0700

    Tạo role constant

---
+++ BIIC-Contest/Constants/RoleConstant.cs
@@ -0,0 +1,10 @@
+﻿namespace BIIC_Contest.Constants
+{
+    public class RoleConstant
+    {
+        public const string ADMIN = "admin";
+        public const string EMPLOYEE = "employee";
+        public const string EXAMINER = "examiner";
+        public const string USER = "user";
+    }
+}
