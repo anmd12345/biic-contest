b091a348480eba30c3f9a8863b14df24ee0230a6
anmd12345 <anvohoang98@gmail.com>
2025-06-07 22:31:48 +0700

    style: fix responsive map box and translate to vietnamese contact page

---
+++ BIIC-Contest/Configs/GoogleMapConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BIIC_Contest.Configs
+{
+    public class GoogleMapConfig
+    {
+        public const string GOOGLE_MAP_API_KEY = "AIzaSyC_rI2C4CGNT__DrDJRKxkfKlhoVwpVpZY";
+    }
+}
