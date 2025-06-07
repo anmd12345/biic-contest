0be372f6d6f11022eb8b27222875fcab0d125b68
anmd12345 <anvohoang98@gmail.com>
2025-06-08 05:14:40 +0700

    Tạo session constant

---
+++ BIIC-Contest/Constants/SessionConstant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BIIC_Contest.Constants
+{
+    public class SessionConstant
+    {
+        public const string CURRENT_USER = "currentUser";
+    }
+}
