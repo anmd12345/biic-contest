867793dc911fd3aeb2ce0a1610d9f94f48009e92
anmd12345 <anvohoang98@gmail.com>
2025-06-07 23:25:04 +0700

    Thêm hàm hỗ trợ lấy ip

--- BIIC-Contest/Repositorys/ContactMessageRepo.cs
+++ BIIC-Contest/Repositorys/ContactMessageRepo.cs
@@ -2,12 +2,14 @@
 
 namespace BIIC_Contest.Repositorys
 {
+    // Các phương thức trong lớp này sẽ tương tác với cơ sở dữ liệu để quản lý thông tin liên hệ
     public class ContactMessageRepo
     {
         private BIICConnectionDbContext db = BIICConnectionDbContext.getInstance;
 
 
         // Method to add a contact message
+
 
     }
 }
