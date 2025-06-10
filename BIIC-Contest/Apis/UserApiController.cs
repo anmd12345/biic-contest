43c81f6c4d7000864adcf7044fefc23224180363
anmd12345 <anvohoang98@gmail.com>
2025-06-10 16:56:47 +0700

    Xây dựng api login

--- BIIC-Contest/Apis/UserApiController.cs
+++ BIIC-Contest/Apis/UserApiController.cs
@@ -1,13 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using BIIC_Contest.Constants;
+using BIIC_Contest.Entitys;
+using BIIC_Contest.Helpers;
+using BIIC_Contest.Services;
+using System;
 using System.Web.Mvc;
 
 namespace BIIC_Contest.Apis
 {
+    [RoutePrefix("apis/v1/user")]
     public class UserApiController : Controller
     {
-        // code here
+        private UserService userService = new UserService();
+
+        [Route("login")]
+        [HttpPost]
+        public JsonResult Login(string username, string password)
+        {
+            try
+            {
+                var response = userService.login(username, password);
+                if (response != null)
+                {
+                    Session[SessionConstant.CURRENT_USER] = response;
+                    return Json(JsonHelper.ConfiguratingJson(response));
+                }
+                return Json(JsonHelper.ConfiguratingJson(new BasicResponseEntity(
+                    false,
+                    "Thông tin đăng nhập không chính xác!"
+                )));
+            }
+            catch (Exception ex)
+            {
+                return Json(JsonHelper.ConfiguratingJson(new BasicResponseEntity(
+                    false,
+                    "Lỗi hệ thống! Không thể đăng nhập bây giờ (" + ex.Message + ")."
+                )));
+            }
+        }
     }
 }
---
+++ BIIC-Contest/scripts/cache-js.js
@@ -0,0 +1,8 @@
+﻿function getLocalStorage(key) {
+    const storedData = localStorage.getItem(key);
+    return storedData ? JSON.parse(storedData) : defaultData;
+}
+
+function saveLocalStorage(key, storedData) {
+    localStorage.setItem(key, JSON.stringify(storedData));
+}
