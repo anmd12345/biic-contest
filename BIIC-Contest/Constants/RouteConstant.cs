eca69ddc869a6fac501b59fc592808e933d64e85
anmd12345 <anvohoang98@gmail.com>
2025-06-10 19:50:24 +0700

    Thêm trang quản lý tin nhắn

--- BIIC-Contest/Constants/RouteConstant.cs
+++ BIIC-Contest/Constants/RouteConstant.cs
@@ -18,5 +18,6 @@
         public const string CONFIG_WEB_PAGE = "/quan-ly-he-thong";
         public const string ACTIVITY_LOG_PAGE = "/quan-ly-he-thong/nhat-ky-hoat-dong";
         public const string USER_MANAGEMENT_PAGE = "/quan-ly-nguoi-dung";
+        public const string CONTACT_MESSAGE_MANAGEMENT_PAGE = "/quan-ly-tin-nhan/danh-sach-tin-nhan";
     }
 }
---
+++ BIIC-Contest/Controllers/Admin/ContactMessageManagementController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BIIC_Contest.Controllers.Admin
+{
+    [RoutePrefix("quan-ly-tin-nhan")]
+    public class ContactMessageManagementController : Controller
+    {
+        [Route("danh-sach-tin-nhan")]
+        public ActionResult ListContactMessage()
+        {
+            return View();
+        }
+
+        [Route("tin-nhan-chi-tiet")]
+        public ActionResult ContactMessageDetail()
+        {
+            return View();
+        }
+    }
+}
