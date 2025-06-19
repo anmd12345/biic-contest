800f22cd8370c972ee74fdd75906e55a29e72e9b
anmd12345 <anvohoang98@gmail.com>
2025-06-19 14:15:10 +0700

    Thêm giao diện danh sách bài dự thi, giao diện chấm điểm, giao diện duyệt bài dự thi

---
+++ BIIC-Contest/Controllers/Admin/SubmissionManagementController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BIIC_Contest.Controllers.Admin
+{
+    public class SubmissionManagementController : Controller
+    {
+        [Route("danh-sach-bai-du-thi")]
+        public ActionResult ListSubmission()
+        {
+            return View();
+        }
+
+
+        [Route("chi-tiet-bai-du-thi-v2")]
+        public ActionResult SubmissionDetail()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ BIIC-Contest/Controllers/SubmissionController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace BIIC_Contest.Controllers
+{
+    public class SubmissionController : Controller
+    {
+        [Route("chi-tiet-bai-du-thi-v1")]
+        public ActionResult SubmissionDetail()
+        {
+            return View();
+        }
+    }
+}
