9c45f6d0e2082b786dc66793a822105c25b195fe
anmd12345 <anvohoang98@gmail.com>
2025-06-23 14:48:56 +0700

    Cập nhật hàm tạo mô tả log

--- BIIC-Contest/Entitys/ActivityLogMessageEntity.cs
+++ BIIC-Contest/Entitys/ActivityLogMessageEntity.cs
@@ -9,6 +9,11 @@ namespace BIIC_Contest.Entitys
         public static readonly Dictionary<int, string> Messages = new Dictionary<int, string>
         {
             {(int) ActivityLogTypeConstant.LOGGED_SUCCESSFULLY, "Đăng nhập thành công" },
+            {(int) ActivityLogTypeConstant.LOGGED_FAILED, "Đăng nhập thất bại" },
+            {(int) ActivityLogTypeConstant.REGISTERED_SUCCESSFULLY, "Đăng ký thành công" },
+            {(int) ActivityLogTypeConstant.REGISTERED_CONTEST, "Đăng ký cuộc thi" },
+            {(int) ActivityLogTypeConstant.UPDATED_WEBSITE_INORMATION, "Cập nhật thông tin website" },
+            {(int) ActivityLogTypeConstant.SEND_CONTACT_MESSAGE, "Gửi tin nhắn liên hệ" }
         };
 
         public static string getMessage(int key)
@@ -21,9 +26,9 @@ namespace BIIC_Contest.Entitys
             return getMessage((int)type);
         }
 
-        public static string getMessage(string author, ActivityLogTypeConstant type)
+        public static string getMessage(string author, ActivityLogTypeConstant type, string des)
         {
-            return $"{author} - {getMessage(type)}";
+            return $"{author} - {getMessage(type)} {des}";
         }
     }
 }
