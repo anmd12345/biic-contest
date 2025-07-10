65e1cf728cfd7acacc02aa750cdcf6b52608dc08
namngxthanh <nam.ngx163@gmail.com>
2025-07-10 13:42:17 +0700

    Create examiner login and examinerpage

---
+++ BIIC-Contest/Controllers/Examniner/BaseExaminerController.cs
@@ -0,0 +1,23 @@
+﻿using BIIC_Contest.Constants;
+using BIIC_Contest.Dtos;
+using System.Web.Mvc;
+
+namespace BIIC_Contest.Controllers
+{
+    public class BaseExaminerController : Controller
+    {
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var currentUser = Session[SessionConstant.CURRENT_USER] as UserDto;
+
+            // Chuyển hướng nếu chưa đăng nhập hoặc không phải là Examiner
+            if (currentUser == null || currentUser.Role.RoleId != 2)
+            {
+                filterContext.Result = new RedirectResult("/dang-nhap");
+                return;
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
